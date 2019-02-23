c18b54d948de7d1fe0bdca379229bdf91a272487
Danilo Carvalho <danilogga@gmail.com>
2019-02-23 15:57:01 -0400

    update current user to be article creator

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,8 +17,8 @@ class ArticlesController < ApplicationController
   def create
     #render plain: params[:article].inspect
     @article = Article.new(article_params)
-    # temporario
-    @article.user = User.first
+
+    @article.user = current_user
 
     if @article.save
       flash[:success] = "Article was successfully created"
